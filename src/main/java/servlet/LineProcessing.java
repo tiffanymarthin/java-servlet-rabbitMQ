53d0b68f5347a9666de261e9d96b91dc318fb999
Tiffany Marthin <tiffanymarthin@gmail.com>
2021-06-03 15:20:26 -0700

    Add simple line processing to count the word frequency and return json object

--- src/main/java/servlet/LineProcessing.java
+++ src/main/java/servlet/LineProcessing.java
@@ -1,2 +1,29 @@
-package servlet;public class LineProcessing {
+package servlet;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LineProcessing {
+
+  public static JsonObject processLine(String line) {
+    String[] arr = line.split(" ");
+    Map<String, Integer> wordCount = new HashMap<>();
+    for (String word : arr) {
+      wordCount.put(word, wordCount.getOrDefault(word, 0) + 1);
+    }
+
+    Gson gson = new Gson();
+    JsonObject jsonMap = gson.toJsonTree(wordCount).getAsJsonObject();
+    System.out.println(jsonMap.size());
+    return jsonMap;
+  }
+
+//  public static void main(String[] args) {
+//    String line = "Testing % this testing testing word % ok loh deh";
+//    JsonObject result = processLine(line);
+//    System.out.println(result);
+//    System.out.println(result.size());
+//  }
 }
